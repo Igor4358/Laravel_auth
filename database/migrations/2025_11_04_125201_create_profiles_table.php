86ba08d8d4760fcd912b94dce7d818eb25fa2950
Igor4358 <igor4358@yandex.ru>
2025-11-07 18:45:05 +0300

    Update 2025_11_04_125201_create_profiles_table.php

--- database/migrations/2025_11_04_125201_create_profiles_table.php
+++ database/migrations/2025_11_04_125201_create_profiles_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('avatar')->nullable();
-            $table->text('bio')->nullable();
             $table->timestamps();
         });
     }
